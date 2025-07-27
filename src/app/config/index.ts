1a705d7f6dabb9c4ed827e91eeb043e5f7c767bc
Vyacheslav <korzan.va@mail.ru>
2025-07-27 18:36:40 +0300

    updated version to 1.19.0

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,5 +1,5 @@
 export const cfg = {
-  VERSION         : '1.17.2',
+  VERSION         : '1.19.0',
   COOKIE_NAME     : 'rhythm',
   SITE_URL        : 'https://rhy.thm.su',
   SITE_TITLE_FULL : 'Rhythm',
