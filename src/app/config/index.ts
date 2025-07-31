2c50bb589671b080bfd5a541e3f94603012d49db
Vyacheslav <korzan.va@mail.ru>
2025-07-31 13:17:16 +0300

    updated version to 1.22.0

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,5 +1,5 @@
 export const cfg = {
-  VERSION         : '1.21.1',
+  VERSION         : '1.22.0',
   COOKIE_NAME     : 'rhythm',
   SITE_URL        : 'https://rhy.thm.su',
   SITE_TITLE_FULL : 'Rhythm',
