afb6f2feed736db5347692bbfbc135006d84fbaa
Vyacheslav <korzan.va@mail.ru>
2025-08-22 12:52:17 +0300

    updated version to 1.36.0

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,5 +1,5 @@
 export const cfg = {
-  VERSION         : '1.35.0',
+  VERSION         : '1.36.0',
   COOKIE_NAME     : 'rhythm',
   SITE_URL        : 'https://rhy.thm.su',
   SITE_TITLE_FULL : 'Rhythm',
